3a669e5cccb87bdb837d70f8c5f4dafe0c13e04b
Ango <ango.gomes@gmail.com>
2021-02-07 20:10:34 +0100

    Added user registration

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,6 @@
 """easy_wod URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -9,15 +8,18 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including another URLconfî
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+
+from django.urls import path
+from django.conf.urls import include
+from django.conf import settings
+from . import views
+
+app_name: str = "user"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path(r"", include("website.urls")),
-    path(r"user/", include("user.urls")),
+    path(r"register", views.RegistrationView.as_view(), name="register"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
+from django.views.generic import FormView
+from .forms import RegisterForm
+from .models import User
+import datetime
+
 
 # Create your views here.
+
+
+class RegistrationView(FormView):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, "user/register.html", {"form": RegisterForm()})
+
+    def post(self, request: HttpRequest) -> HttpResponse:
+        """Manages the user registration.
+        """
+
+        email: str = request.POST.get("email")
+        password: str = request.POST.get("password1")
+        first_name: str = request.POST.get("first_name")
+        last_name: str = request.POST.get("last_name")
+        date_of_birth = f'{request.POST.get("date_of_birth_year")}-{request.POST.get("date_of_birth_month")}-{request.POST.get("date_of_birth_day")}'
+
+        user: User = User.objects.create_user(
+            email=email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+            date_of_birth=date_of_birth,
+        )
+        login(self.request, user)
+        return redirect("/")
