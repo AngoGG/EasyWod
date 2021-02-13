ad7fa66064954fcfaa78148f44168afda9a72379
Ango <ango.gomes@gmail.com>
2021-02-13 18:19:02 +0100

    Article Model with basic view/template

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from user.models import User
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(_("Titre"), max_length=255)
+    author = models.ForeignKey(User, verbose_name=_("Auteur"), on_delete=models.CASCADE)
+    body = models.TextField(_("Contenu"))
+
+    def __str__(self):
+        return self.title + " | " + str(self.author)
+
