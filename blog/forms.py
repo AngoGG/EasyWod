ec6091ac12a2d08d92a1e10ed2505781d9b99751
Ango <ango.gomes@gmail.com>
2021-02-14 01:59:25 +0100

    Now fill in Article author automatically

--- blog/forms.py
+++ blog/forms.py
@@ -9,6 +9,7 @@ class ArticleForm(forms.ModelForm):
 
         widgets = {
             "title": forms.TextInput(attrs={"placeholder": "Titre de l'article"}),
+            "author": forms.TextInput(attrs={"value": "", "type": "hidden"}),
             "body": forms.TextInput(
                 attrs={"placeholder": "Entrez le contenu de votre Article"}
             ),
