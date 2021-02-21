676b3722f7e32192631eea50fadb395f16928d10
Ango <ango.gomes@gmail.com>
2021-02-21 18:36:15 +0100

    Event with registration access test

--- event/tests/test_event_views.py
+++ event/tests/test_event_views.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from django.db.models.query import QuerySet
 from django.http import HttpResponse
 from django.test import Client, TestCase
-from event.models import Event
+from event.models import Event, EventMember
+from user.models import User
 
 
 class TestCalendarView(TestCase):
@@ -14,15 +15,35 @@ class TestCalendarView(TestCase):
 
 class TestEventView(TestCase):
     def test_event_without_registration_access(self) -> None:
-        name = "WOD"
-        slot = "1"
-        start = datetime.now()
-        end = datetime.now()
-
-        event = Event.objects.create(name=name, start=start, end=end, slot=slot)
-        event.save()
+        Event.objects.create(
+            name="WOD", start=datetime.now(), end=datetime.now(), slot="1"
+        )
 
         event_created: QuerySet = Event.objects.first()
+        client: Client = Client()
+        response: HttpResponse = client.get(f"/event/{event_created.pk}",)
+
+        assert response.status_code == 200  # Testing redirection
+
+    def test_event_with_registration_access(self) -> None:
+        User.objects.create_user(
+            email="matt-fraser@gmail.com",
+            password="password8chars",
+            first_name="Matt",
+            last_name="Fraser",
+            date_of_birth="1997-4-10",
+        )
+
+        user_created: QuerySet = User.objects.first()  # type: ignore
+
+        Event.objects.create(
+            name="WOD", start=datetime.now(), end=datetime.now(), slot="1"
+        )
+
+        event_created: QuerySet = Event.objects.first()
+
+        EventMember.objects.create(event=event_created, user=user_created)
+
         client: Client = Client()
         response: HttpResponse = client.get(f"/event/{event_created.pk}",)
 
