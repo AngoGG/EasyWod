4d80d036a0a19831ed150f53086a9cce12adb1ef
Ango <ango.gomes@gmail.com>
2021-03-10 20:33:17 +0100

    css/js cleaning up

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -12,14 +12,6 @@ Created: colorlib
 (function ($) {
 
     /*------------------
-        Preloader
-    --------------------*/
-    $(window).on('load', function () {
-        $(".loader").fadeOut();
-        $("#preloder").delay(200).fadeOut("slow");
-    });
-
-    /*------------------
         Background Set
     --------------------*/
     $('.set-bg').each(function () {
@@ -33,19 +25,6 @@ Created: colorlib
     $(".mobile-menu").slicknav({
         prependTo: '#mobile-menu-wrap',
         allowParentLinks: true
-    });
-
-    /*------------------
-		Search model
-	--------------------*/
-    $('.search-trigger').on('click', function () {
-        $('.search-model').fadeIn(400);
-    });
-
-    $('.search-close-switch').on('click', function () {
-        $('.search-model').fadeOut(400, function () {
-            $('#search-input').val('');
-        });
     });
 
     /*------------------
@@ -69,64 +48,6 @@ Created: colorlib
             }
         });
     });
-
-    /*------------------
-        Elements Counter UP
-    --------------------*/
-    $('.mile-counter').each(function () {
-        $(this).prop('Counter', 0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 4000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
-    });
-
-    /*------------------
-        Accordin Active
-    --------------------*/
-    $('.collapse').on('shown.bs.collapse', function () {
-        $(this).prev().addClass('active');
-    });
-
-    $('.collapse').on('hidden.bs.collapse', function () {
-        $(this).prev().removeClass('active');
-    });
-
-    /*------------------
-        Progress Loader
-    --------------------*/
-    $('.circle-progress').each(function () {
-        var cpvalue = $(this).data("cpvalue");
-        var cpcolor = $(this).data("cpcolor");
-        var cpid = $(this).data("cpid");
-
-        $(this).append('<div class="' + cpid + '"></div><div class="progress-value"><span class="loader-percentage">' + cpvalue + '<sup class="percentage-sign">%</sup></span></div>');
-
-        if (cpvalue < 100) {
-
-            $('.' + cpid).circleProgress({
-                value: '0.' + cpvalue,
-                size: 190,
-                thickness: 5,
-                startAngle: -190,
-                fill: cpcolor,
-                emptyFill: "rgba(0, 0, 0, 0)"
-            });
-        } else {
-            $('.' + cpid).circleProgress({
-                value: 1,
-                size: 187,
-                thickness: 5,
-                fill: cpcolor,
-                emptyFill: "rgba(0, 0, 0, 0)"
-            });
-        }
-    });
-
 
 
 })(jQuery);
