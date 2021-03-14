f01b5ecd7e56d0bb4112f310322690e8eb179e11
Ango <ango.gomes@gmail.com>
2021-03-14 14:59:50 +0100

    Prepared membership queries for employee homepage

---
+++ membership/libs/membership_queries.py
@@ -0,0 +1,29 @@
+from membership.models import Membership, UserMembership, Subscription
+
+
+def get_all_active_premium_membership():
+    active_premium_members = Subscription.objects.filter(active=True).filter(
+        user_membership__membership__membership_type="PREMIUM"
+    )
+    return active_premium_members
+
+
+def get_all_inactive_premium_membership():
+    inactive_premium_members = Subscription.objects.filter(active=False).filter(
+        user_membership__membership__membership_type="PREMIUM"
+    )
+    return inactive_premium_members
+
+
+def get_all_active_trial_membership():
+    active_trial_members = Subscription.objects.filter(active=True).filter(
+        user_membership__membership__membership_type="TRIAL"
+    )
+    return active_trial_members
+
+
+def get_all_inactive_trial_membership():
+    inactive_trial_members = Subscription.objects.filter(active=False).filter(
+        user_membership__membership__membership_type="TRIAL"
+    )
+    return inactive_trial_members
