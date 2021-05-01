f58ec37448cbbb0aece163da3bdfad8d82dd5066
Ango <ango.gomes@gmail.com>
2021-05-01 11:00:06 +0200

    Added answer to contact model

--- contact_us/models.py
+++ contact_us/models.py
@@ -6,8 +6,9 @@ class ContactMessage(models.Model):
     name = models.CharField(_("Nom"), max_length=50)
     email = models.EmailField(_("Adresse Mail"), max_length=254)
     subject = models.CharField(_("Sujet"), max_length=50)
-    message = models.CharField(_("Message"), max_length=50)
+    message = models.CharField(_("Message"), max_length=500)
     message_date = models.DateTimeField(_("Date du message"), auto_now_add=True)
+    answer = models.CharField(_("Réponse"), max_length=500, null=True)
     answer_date = models.DateTimeField(_("Date de réponse"), null=True)
 
     def __str__(self):
