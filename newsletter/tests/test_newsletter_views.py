264ee0845bb74a8265fe6f1daa6a42083bcf2aab
Ango <ango.gomes@gmail.com>
2021-05-01 18:47:12 +0200

    Added redirection assertion

--- newsletter/tests/test_newsletter_views.py
+++ newsletter/tests/test_newsletter_views.py
@@ -1,5 +1,8 @@
 from django.db.models.query import QuerySet
 from django.test import Client, TestCase
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
 from newsletter.models import SubscribedUsers
 
 
@@ -7,10 +10,9 @@ class TestSubscribeView(TestCase):
     def test_subscribe(self):
         # Sub
         client: Client = Client(HTTP_HOST="localhost")
-        client.post(
-            "/newsletter/", {"email": ["matt-fraser@gmail.com"]},
-        )
+        response = client.post("/newsletter/", {"email": ["matt-fraser@gmail.com"]},)
         # Check mail
         subscribers = SubscribedUsers.objects.first()
         assert subscribers.email == "matt-fraser@gmail.com"
+        assert response.status_code == 302  # Testing redirection
 
