e36e2c7f7bed02425fb0945eee355b07833a588f
Ango <ango.gomes@gmail.com>
2021-05-01 13:04:22 +0200

    A user can now unsubscribe from the newsletter

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -5,5 +5,6 @@ app_name = 'newsletter'
 
 urlpatterns = [
     path(r"", views.SubscribeView.as_view(), name='subscribe'),
+    path(r"delete/<uidb64>", views.UnsubscribeView.as_view(), name="unsubscribe"),
 ]
 
