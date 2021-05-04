60216368a9e073b684ce32a52cab5c6d4e3abdcb
Ango <ango.gomes@gmail.com>
2021-05-04 13:19:55 +0200

    UserMembership creation view test

--- membership/urls.py
+++ membership/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'membership'
 
 urlpatterns = [
+    path(r"create/<uid>", views.CreateUserMembership.as_view(), name="create"),
     path(r"<int:pk>", views.UserMembershipView.as_view(), name='select'),
     path(r"", views.UpdateMemberShipView.as_view(), name='change'),
     path(r"deactivate", views.DeactivateMemberShipView.as_view(), name='deactivate',),
