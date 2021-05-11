a6d1a5eb7f397ef4f72620b460fff9507e520bae
Ango <ango.gomes@gmail.com>
2021-05-11 21:47:44 +0200

    Code refactoring + Typing and Docstrings

--- membership/libs/membership_queries.py
+++ membership/libs/membership_queries.py
@@ -1,39 +1,39 @@
+from typing import List
+
+from django.db.models.query import QuerySet
 from django.utils import timezone
 
 from membership.models import UserMembership
 
 
-def get_all_active_premium_membership():
-    print(f'HELLO')
-    active_premium_members = UserMembership.objects.filter(active=True).filter(
+def get_all_active_premium_membership() -> QuerySet:
+    return UserMembership.objects.filter(active=True).filter(
         membership__membership_type="PREMIUM"
     )
-    print(f'active_premium_members : {active_premium_members}')
-    return active_premium_members
 
 
-def get_all_inactive_premium_membership():
-    inactive_premium_members = UserMembership.objects.filter(active=False).filter(
+def get_all_inactive_premium_membership() -> QuerySet:
+    return UserMembership.objects.filter(active=False).filter(
         membership__membership_type="PREMIUM"
     )
-    return inactive_premium_members
 
 
-def get_all_active_trial_membership():
-    active_trial_members = UserMembership.objects.filter(active=True).filter(
+def get_all_active_trial_membership() -> QuerySet:
+    return UserMembership.objects.filter(active=True).filter(
         membership__membership_type="TRIAL"
     )
-    return active_trial_members
 
 
-def get_all_inactive_trial_membership():
-    inactive_trial_members = UserMembership.objects.filter(active=False).filter(
+def get_all_inactive_trial_membership() -> QuerySet:
+    return UserMembership.objects.filter(active=False).filter(
         membership__membership_type="TRIAL"
     )
-    return inactive_trial_members
 
 
-def get_all_active_membership():
+def get_all_active_membership() -> List[str]:
+    '''Get all the active membership and returns their user email.
+    '''
+
     active_membership = UserMembership.objects.filter(active=True)
     active_member_list = []
     for membership in active_membership:
@@ -41,17 +41,19 @@ def get_all_active_membership():
     return active_member_list
 
 
-def get_trial_to_deactivate():
-    user_memberships = UserMembership.objects.filter(
+def get_trial_to_deactivate() -> QuerySet:
+    '''Returns all the active trial membership 
+    with no remaining course and returns.
+    '''
+    return UserMembership.objects.filter(
         membership__membership_type="TRIAL", active=True, remaining_trial_courses=0,
     )
-    user_email_list = []
-    for user_membership in user_memberships:
-        user_email_list.append(user_membership)
-    return user_email_list
 
 
-def get_previous_day_ended_trial():
+def get_previous_day_ended_trial() -> List[str]:
+    '''Get all trial than have been deactivated the previous day 
+    and returns their user mail
+    '''
     previous_day = timezone.now() - timezone.timedelta(1)
     user_memberships = UserMembership.objects.filter(
         membership__membership_type="TRIAL",
